73f25cda5bda447bc40820a4108d429fe4030996
Michael Zabka <zabka.michael@gmail.com>
2017-02-20 19:05:30 +0100

    Change interface of eventQueue to return promise instead of async iterator
    * to allow use async call without framework co

--- src/RabbitMQ/EventSourcing/eventQueue.ts
+++ src/RabbitMQ/EventSourcing/eventQueue.ts
@@ -4,36 +4,42 @@ import IEvent from './IEvent';
 
 const QUEUE_NAME_PREFIX = 'events.';
 
-export function* enqueueList(conn: Connection, events: IEvent[]) {
+export async function enqueueList(conn: Connection, events: IEvent[]) {
 	for (let event of events) {
-		yield enqueue(conn, event);
+		await enqueue(conn, event);
 	}
 }
 
-export function* enqueue(conn: Connection, event: IEvent) {
+export async function enqueue(conn: Connection, event: IEvent) {
 	const queueName = QUEUE_NAME_PREFIX + event.type;
-	const channel = yield conn.createChannel();
-	yield channel.assertQueue(queueName);
-	channel.sendToQueue(queueName, new Buffer(JSON.stringify(event)), { parsistent: true });
+	const channel = await conn.createChannel();
+	await channel.assertQueue(queueName);
+	channel.sendToQueue(queueName, new Buffer(JSON.stringify(event)), { persistent: true });
 }
 
-export function* fetchNext(conn: Connection, eventType: string) {
+export async function fetchNext(conn: Connection, eventType: string) {
 	const queueName = QUEUE_NAME_PREFIX + eventType;
-	const channel = yield conn.createChannel();
-	yield channel.assertQueue(queueName);
-	const message: Message = yield channel.get(queueName, { noAck: true });
-	return message.content ? JSON.parse(message.content.toString()) : null;
+	const channel = await conn.createChannel();
+	await channel.assertQueue(queueName);
+	const message: Message | boolean = await channel.get(queueName, { noAck: true });
+	if (message && typeof message !== 'boolean') {
+		return message.content ? JSON.parse(message.content.toString()) : null;
+	} else {
+		return null;
+	}
 }
 
-export function* bindMore(conn: Connection, eventTypes: string[], onEvent: (event: IEvent, onProcessed: () => void) => void) {
-	yield eventTypes.map((eventType: string) => bindOne(conn, eventType, onEvent));
+export async function bindMore(conn: Connection, eventTypes: string[], onEvent: (event: IEvent, onProcessed: () => void) => void) {
+	for (let eventType of eventTypes) {
+		await bindOne(conn, eventType, onEvent);
+	}
 }
 
-export function* bindOne(conn: Connection, eventType: string, onEvent: (event: IEvent, onProcessed: () => void) => void) {
+export async function bindOne(conn: Connection, eventType: string, onEvent: (event: IEvent, onProcessed: () => void) => void) {
 	const queueName = QUEUE_NAME_PREFIX + eventType;
-	const channel = yield conn.createChannel();
-	yield channel.assertQueue(queueName);
-	yield channel.consume(queueName, (message: Message) => {
+	const channel = await conn.createChannel();
+	await channel.assertQueue(queueName);
+	await channel.consume(queueName, (message: Message) => {
 		try {
 			const event = JSON.parse(message.content.toString());
 			onEvent(event, () => channel.ack(message));
