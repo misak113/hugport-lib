94ffd140e4cbf441e65c22ca44faf46b9523ea6b
Michael Zabka <zabka.michael@gmail.com>
2017-03-09 10:28:07 +0100

    Allow use credentials in rethink connection

--- src/RethinkDB/rethinkDBConnectionFactory.ts
+++ src/RethinkDB/rethinkDBConnectionFactory.ts
@@ -10,10 +10,13 @@ export interface IRethinkDBConnection {
 
 export function createRethinkDBConnection(rethinkDsn: string): IRethinkDBConnection {
 	const url = parse(rethinkDsn);
+	const credentials = url.auth && url.auth.split(':');
 	const options = {
 		host: url.hostname,
 		port: url.port,
 		db: url.pathname.substr(1),
+		username: credentials && credentials[0],
+		password: credentials && credentials[1],
 	};
 	const self = {
 		connection: null,
