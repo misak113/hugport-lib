e672a41cdf361f9ed549f37abf7274e6436a0398
Michael Zabka <zabka.michael@gmail.com>
2017-03-21 11:29:57 +0100

    Add Raven logger implementation as additional for rollbar

---
+++ libs/raven/raven-node.d.ts
@@ -0,0 +1,4 @@
+
+declare module 'raven' {
+  export = Raven;
+}
--- src/Logging/logger.ts
+++ src/Logging/logger.ts
@@ -1,4 +1,6 @@
 
+import * as Raven from 'raven';
+
 export function useRollbarLogging(rollbar: any) {
 	// override default console logging
 	const consoleWarn = console.warn;
@@ -17,4 +19,38 @@ export function useRollbarLogging(rollbar: any) {
 			{ level: 'error', custom: { message, optionalParams } }
 		);
 	};
+}
+
+export function useRavenLogging() {
+	// override default console logging
+	const consoleWarn = console.warn;
+	const consoleError = console.error;
+	console.warn = (message?: any, ...optionalParams: any[]) => {
+		consoleWarn.call(console, message, ...optionalParams);
+		captureRaven('warning', 'warn', message, ...optionalParams);
+	};
+	console.error = (message?: any, ...optionalParams: any[]) => {
+		consoleError.call(console, message, ...optionalParams);
+		captureRaven('error', 'error', message, ...optionalParams);
+	};
+}
+
+function captureRaven(level: string, source: string, message: any, ...optionalParams: any[]) {
+	const options = {
+		level,
+		tags: {
+			source,
+		},
+		extra: optionalParams,
+	};
+	if (message instanceof Error) {
+		Raven.captureException(message, options);
+	} else {
+		if (typeof message === 'string') {
+			message = message;
+		} else {
+			message = JSON.stringify(message);
+		}
+		Raven.captureMessage(message, options);
+	}
 }
