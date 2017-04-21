67df22a00add2950a97c34f5af520f3988079440
Michael Zabka <zabka.michael@gmail.com>
2017-04-22 00:36:01 +0200

    Make commands possibly to process instead of just enqueue it
    * process commands will expecting response from command handler which is consuming messages

--- src/AMQP/CQRS/commandQueue.ts
+++ src/AMQP/CQRS/commandQueue.ts
@@ -8,12 +8,40 @@ const OPTIONS = {
 	confirmable: true,
 };
 
+export type IResponseMessage = IProcessSucceedMessage | IProcessFailedMessage | IErrorMessage;
+
+export interface IProcessSucceedMessage {
+	status: 'process_succeed';
+	command: ICommand;
+}
+
+export interface IProcessFailedMessage {
+	status: 'process_failed';
+	command: ICommand;
+	errorType: string;
+	errorMessage: string;
+}
+
+export interface IErrorMessage {
+	status: 'error';
+	error: any;
+}
+
 export async function enqueue(amqpConnection: IAMQPConnection, command: ICommand) {
 	const queueName = QUEUE_NAME;
 	await amqpConnection.queuePublisher.enqueueRepeatable(queueName, command, OPTIONS);
 }
 
-export async function bindAll(amqpConnection: IAMQPConnection, onCommand: (command: ICommand) => Promise<void>) {
+export async function process(amqpConnection: IAMQPConnection, command: ICommand) {
+	const queueName = QUEUE_NAME;
+	return await amqpConnection.queuePublisher.enqueueExpectingResponseRepeatable<ICommand, IResponseMessage>(
+		queueName,
+		command,
+		OPTIONS
+	);
+}
+
+export async function bindAll(amqpConnection: IAMQPConnection, onCommand: (command: ICommand) => Promise<IResponseMessage | undefined>) {
 	const queueName = QUEUE_NAME;
 	await amqpConnection.queueSubscriber.subscribeRepeatable(queueName, onCommand, OPTIONS);
 }
