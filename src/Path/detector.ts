5aebf68ee6e9bc7e5bee0dd69fb6ea86bebb5596
Michael Zabka <zabka.michael@gmail.com>
2017-09-05 21:02:47 +0200

    Allow use root path detector also for browsers with null returning

--- src/Path/detector.ts
+++ src/Path/detector.ts
@@ -1,8 +1,13 @@
 
-import * as fs from 'fs';
 import * as path from 'path';
 
 export function detectModuleRootPath(moduleName: string, deepLimit: number = 10, basePath: string = __dirname) {
+	let fs: any;
+	try {
+		fs = require('fs');
+	} catch (error) {
+		return null;
+	}
 	for (let i = 0; i < deepLimit; i++) {
 		const packagePath = path.join(basePath, 'package.json');
 		if (fs.existsSync(packagePath) && JSON.parse(fs.readFileSync(packagePath).toString()).name === moduleName) {
