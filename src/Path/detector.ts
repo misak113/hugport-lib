fb1232ac7835bbde38fed1607dd452042ba07b89
Michael Zabka <zabka.michael@gmail.com>
2017-09-05 18:22:36 +0200

    Use fs instead of classic require to check content of package.json

--- src/Path/detector.ts
+++ src/Path/detector.ts
@@ -6,7 +6,7 @@ export function detectModuleRootPath(moduleName: string, deepLimit: number = 10)
 	let basePath = __dirname;
 	for (let i = 0; i < deepLimit; i++) {
 		const packagePath = path.join(basePath, 'package.json');
-		if (fs.existsSync(packagePath) && require(packagePath).name === moduleName) {
+		if (fs.existsSync(packagePath) && JSON.parse(fs.readFileSync(packagePath).toString()).name === moduleName) {
 			return path.resolve(basePath);
 		}
 		basePath = path.join(basePath, '..');
