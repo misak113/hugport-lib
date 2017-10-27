f37cf0102ee57c5903c34cd8d4a09e59ddb5d3d0
Michael Zabka <zabka.michael@gmail.com>
2017-10-27 20:15:58 +0200

    Better overriding of timestamp by async getter

--- src/DateTime/dateMonkeyPatch.ts
+++ src/DateTime/dateMonkeyPatch.ts
@@ -62,16 +62,37 @@ export function overrideDateCurrentTimestampInSecondsByAsync<TGlobal extends { D
 	updatingInterval: number = 2e3 // in milliseconds
 ) {
 	let lastCurrentTimestamp: number;
-	let updatedAtSeconds: number = 0;
+	let updatedAt: number = 0;
+	let incremented: number = 0;
 	const OriginalDate: DateConstructor = overrideDateCurrentTimestamp(
 		root,
-		() => lastCurrentTimestamp
-			+ (Math.floor(OriginalDate.now() / 1e3) - updatedAtSeconds) * 1e3 // last update based on seconds
-			+ OriginalDate.now() % 1e3, // keep ms from internal clock
+		() => {
+			const millisecondsSinceLastUpdate = OriginalDate.now() - updatedAt;
+			return lastCurrentTimestamp
+				- incremented
+				+ millisecondsSinceLastUpdate;
+		}
 	);
 	const updateCurrentTimestamp = async () => {
-		lastCurrentTimestamp = await getCurrentTimestampSeconds() * 1e3;
-		updatedAtSeconds = Math.floor(OriginalDate.now() / 1e3);
+		const newCurrentTimestamp = await getCurrentTimestampSeconds() * 1e3;
+		const secondsDifference = newCurrentTimestamp - lastCurrentTimestamp;
+
+		if (secondsDifference > 0) {
+			incremented = incremented + secondsDifference;
+		}
+
+		const millisecondsSinceLastUpdate = OriginalDate.now() - updatedAt;
+		if (incremented > 0 && millisecondsSinceLastUpdate >= 1e3) {
+			incremented = 0;
+			updatedAt = updatedAt % 1e3;
+		}
+
+		if (secondsDifference < 0) {
+			incremented = 0;
+			updatedAt = OriginalDate.now();
+		}
+
+		lastCurrentTimestamp = newCurrentTimestamp;
 	};
 	setInterval(updateCurrentTimestamp, updatingInterval);
 	updateCurrentTimestamp();
@@ -84,16 +105,37 @@ export function overrideDateCurrentTimestampInMinutesByAsync<TGlobal extends { D
 	updatingInterval: number = 10e3 // in milliseconds
 ) {
 	let lastCurrentTimestamp: number;
-	let updatedAtMinutes: number = 0;
+	let updatedAt: number = 0;
+	let incremented: boolean = false;
 	const OriginalDate: DateConstructor = overrideDateCurrentTimestamp(
 		root,
-		() => lastCurrentTimestamp
-			+ (Math.floor(OriginalDate.now() / 60e3) - updatedAtMinutes) * 60e3 // last update based on seconds
-			+ OriginalDate.now() % 60e3, // keep ms from internal clock
+		() => {
+			const millisecondsSinceLastUpdate = OriginalDate.now() - updatedAt;
+			return lastCurrentTimestamp
+				+ (incremented ? -1 : 0)
+				+ millisecondsSinceLastUpdate;
+		}
 	);
 	const updateCurrentTimestamp = async () => {
-		lastCurrentTimestamp = await getCurrentTimestampMinutes() * 60e3;
-		updatedAtMinutes = Math.floor(OriginalDate.now() / 60e3);
+		const newCurrentTimestamp = await getCurrentTimestampMinutes() * 60e3;
+		const minutesDifference = newCurrentTimestamp - lastCurrentTimestamp;
+
+		if (minutesDifference === 1) {
+			incremented = true;
+		}
+
+		const millisecondsSinceLastUpdate = OriginalDate.now() - updatedAt;
+		if (incremented && millisecondsSinceLastUpdate >= 60e3) {
+			incremented = false;
+			updatedAt = updatedAt - 60e3;
+		}
+
+		if (minutesDifference < 0 || minutesDifference > 1) {
+			incremented = false;
+			updatedAt = OriginalDate.now();
+		}
+
+		lastCurrentTimestamp = newCurrentTimestamp;
 	};
 	setInterval(updateCurrentTimestamp, updatingInterval);
 	updateCurrentTimestamp();
