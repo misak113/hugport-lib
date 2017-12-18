2a3423c145f4c9b866a126fe05afe756764221eb
Michael Zabka <zabka.michael@gmail.com>
2017-12-18 19:42:09 +0100

    Add binding of events with prefetched count in some timeout
    * it will allow consume more events in one batch & process them in batch

--- src/AMQP/EventSourcing/eventQueue.ts
+++ src/AMQP/EventSourcing/eventQueue.ts
@@ -3,6 +3,7 @@ import { IAMQPConnection } from '../amqpConnectionFactory';
 import fetchNextMessage from '../fetchNextMessage';
 import INackOptions from '../INackOptions';
 import IEvent, { IEventPayload } from './IEvent';
+import * as _ from 'lodash';
 
 const QUEUE_NAME_PREFIX = 'events.';
 const OPTIONS = {
@@ -50,6 +51,60 @@ export async function bindOneExpectingConfirmation<TPayload extends IEventPayloa
 ) {
 	const queueName = QUEUE_NAME_PREFIX + eventType;
 	return await amqpConnection.queueSubscriber.subscribeExpectingConfirmationRepeatable(queueName, onEvent, OPTIONS);
+}
+
+export async function bindPrefetchedExpectingConfirmation<TPayload extends IEventPayload>(
+	amqpConnection: IAMQPConnection,
+	eventType: string,
+	prefetchCount: number,
+	debounceTimeoutMs: number,
+	onEvents: (
+		events: IEvent<TPayload>[],
+		ack: (event: IEvent<TPayload>) => void,
+		nack: (event: IEvent<TPayload>, options?: INackOptions) => void,
+	) => Promise<void>,
+) {
+	interface IEventConsumption {
+		event: IEvent<TPayload>;
+		ack: () => void;
+		nack: (options?: INackOptions) => void;
+	}
+
+	const unconsumedConsumptions: IEventConsumption[] = [];
+	const consumedConsumptions: IEventConsumption[] = [];
+	const ackEvent = (event: IEvent<TPayload>) => {
+		const indexOfEvent = consumedConsumptions.findIndex((consumption: IEventConsumption) => consumption.event === event);
+		const consumption = consumedConsumptions[indexOfEvent];
+		consumedConsumptions.splice(indexOfEvent, 1);
+		consumption.ack();
+	};
+	const nackEvent = (event: IEvent<TPayload>, options?: INackOptions) => {
+		const indexOfEvent = consumedConsumptions.findIndex((consumption: IEventConsumption) => consumption.event === event);
+		const consumption = consumedConsumptions[indexOfEvent];
+		consumedConsumptions.splice(indexOfEvent, 1);
+		consumption.nack(options);
+	};
+	const debouncedOnEvents = _.debounce(
+		() => {
+			let consumption: IEventConsumption | undefined;
+			const events = [];
+			while (consumption = unconsumedConsumptions.shift()) {
+				events.push(consumption.event);
+				consumedConsumptions.push(consumption);
+			}
+			onEvents(events, ackEvent, nackEvent);
+		},
+		debounceTimeoutMs,
+	);
+	const queueName = QUEUE_NAME_PREFIX + eventType;
+	return await amqpConnection.queueSubscriber.subscribeExpectingConfirmationRepeatable(
+		queueName,
+		async (event: IEvent<TPayload>, ack: () => void, nack: (options?: INackOptions) => void) => {
+			unconsumedConsumptions.push({ event, ack, nack });
+			debouncedOnEvents();
+		},
+		{ ...OPTIONS, prefetchCount },
+	);
 }
 
 export async function purgeMore(amqpConnection: IAMQPConnection, eventTypes: string[]) {
