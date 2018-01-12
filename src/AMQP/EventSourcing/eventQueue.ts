ee6331e324b8bad0178442e85c9f5af1c6c01714
Michal Artazov <michal@artazov.cz>
2018-01-12 13:04:54 +0100

    fixup! use AMQP exchanges for routing instead of direct send to queues

--- src/AMQP/EventSourcing/eventQueue.ts
+++ src/AMQP/EventSourcing/eventQueue.ts
@@ -4,53 +4,50 @@ import fetchNextMessage from '../fetchNextMessage';
 import INackOptions from '../INackOptions';
 import IEvent, { IEventPayload } from './IEvent';
 
+const EXCHANGE_NAME = 'events';
 const OPTIONS = {
 	persistent: true,
 	confirmable: true,
 	prefetchCount: 1,
 };
 
-export async function enqueue(amqpConnection: IAMQPConnection, event: IEvent<IEventPayload>, destination: string) {
-	await amqpConnection.queuePublisher.enqueueRepeatable(event, event.type, destination, OPTIONS);
+export async function enqueue(amqpConnection: IAMQPConnection, event: IEvent<IEventPayload>) {
+	await amqpConnection.queuePublisher.enqueueRepeatable(event, event.type, EXCHANGE_NAME, OPTIONS);
 }
 
 export async function fetchNext<TPayload extends IEventPayload>(
 	amqpConnection: IAMQPConnection,
 	eventType: string,
-	destination: string,
 	consumerType: string,
 ): Promise<IEvent<TPayload> | null> {
 	const queueName = getQueueName(consumerType, eventType);
-	return await fetchNextMessage<IEvent<TPayload> | null>(amqpConnection, queueName, eventType, destination);
+	return await fetchNextMessage<IEvent<TPayload> | null>(amqpConnection, queueName, eventType, EXCHANGE_NAME);
 }
 
 export async function bindMore<TPayload extends IEventPayload>(
 	amqpConnection: IAMQPConnection,
 	eventTypes: string[],
-	destination: string,
 	consumerType: string,
 	onEvent: (event: IEvent<TPayload>) => Promise<void>,
 ) {
 	for (let eventType of eventTypes) {
-		await bindOne(amqpConnection, eventType, destination, consumerType, onEvent);
+		await bindOne(amqpConnection, eventType, consumerType, onEvent);
 	}
 }
 
 export async function bindOne<TPayload extends IEventPayload>(
 	amqpConnection: IAMQPConnection,
 	eventType: string,
-	destination: string,
 	consumerType: string,
 	onEvent: (event: IEvent<TPayload>) => Promise<void>,
 ) {
 	const queueName = getQueueName(consumerType, eventType);
-	return await amqpConnection.queueSubscriber.subscribeRepeatable(queueName, onEvent, eventType, destination, OPTIONS);
+	return await amqpConnection.queueSubscriber.subscribeRepeatable(queueName, onEvent, eventType, EXCHANGE_NAME, OPTIONS);
 }
 
 export async function bindOneExpectingConfirmation<TPayload extends IEventPayload>(
 	amqpConnection: IAMQPConnection,
 	eventType: string,
-	destination: string,
 	consumerType: string,
 	onEvent: (event: IEvent<TPayload>, ack: () => void, nack: (options?: INackOptions) => void) => Promise<void>,
 ) {
@@ -59,7 +56,7 @@ export async function bindOneExpectingConfirmation<TPayload extends IEventPayloa
 		queueName,
 		onEvent,
 		eventType,
-		destination,
+		EXCHANGE_NAME,
 		OPTIONS,
 	);
 }
@@ -67,12 +64,11 @@ export async function bindOneExpectingConfirmation<TPayload extends IEventPayloa
 export async function purgeMore(
 	amqpConnection: IAMQPConnection,
 	eventTypes: string[],
-	destination: string,
 	consumerType: string,
 ) {
 	for (let eventType of eventTypes) {
 		/* tslint:disable-next-line */
-		while (await fetchNext(amqpConnection, eventType, destination, consumerType)) ;
+		while (await fetchNext(amqpConnection, eventType, consumerType)) ;
 	}
 }
 
--- tests/integration/AMQP/EventSourcing/eventQueueSpec.ts
+++ tests/integration/AMQP/EventSourcing/eventQueueSpec.ts
@@ -38,26 +38,24 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 
 		it('should send event to an exchange defined by destination with a given routing key', async function () {
 			const channel = await this.amqplibConnection.createChannel();
-			await channel.deleteExchange('destination1');
-			await channel.deleteExchange('destination2');
-			await channel.deleteExchange('destination3');
+			await channel.deleteExchange('events');
 			await channel.deleteQueue('consumer1_event1');
 			await channel.deleteQueue('consumer1_event2');
 			await channel.deleteQueue('consumer2_event1');
-			await channel.deleteQueue('consumer3_event1');
-			await channel.assertExchange('destination1', 'direct');
-			await channel.assertExchange('destination2', 'direct');
-			await channel.assertExchange('destination3', 'direct');
+			await channel.deleteQueue('consumer3_event2');
+			await channel.assertExchange('events', 'direct');
 			await channel.assertQueue('consumer1_event1');
 			await channel.assertQueue('consumer1_event2');
 			await channel.assertQueue('consumer2_event1');
-			await channel.assertQueue('consumer3_event1');
-			await channel.bindQueue('consumer1_event1', 'destination1', 'event1');
-			await channel.bindQueue('consumer1_event2', 'destination1', 'event2');
-			await channel.bindQueue('consumer2_event1', 'destination2', 'event1');
-			await channel.bindQueue('consumer3_event1', 'destination3', 'event1');
+			await channel.assertQueue('consumer3_event2');
+			await channel.bindQueue('consumer1_event1', 'events', 'event1');
+			await channel.bindQueue('consumer1_event2', 'events', 'event2');
+			await channel.bindQueue('consumer2_event1', 'events', 'event1');
+			await channel.bindQueue('consumer3_event2', 'events', 'event2');
 
-			await enqueue(amqpConnection, createEvent('event1', 1), 'destination1');
+			await enqueue(amqpConnection, createEvent('event1', 1));
+			await enqueue(amqpConnection, createEvent('event2', 2));
+
 			const result1 = await channel.get('consumer1_event1');
 			JSON.parse(result1!.content.toString()).should.deepEqual({
 				...createEvent('event1', 1),
@@ -65,7 +63,6 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 				receivedAt: new Date(2018, 0, 1).toISOString(),
 			});
 
-			await enqueue(amqpConnection, createEvent('event2', 2), 'destination1');
 			const result2 = await channel.get('consumer1_event2');
 			JSON.parse(result2!.content.toString()).should.deepEqual({
 				...createEvent('event2', 2),
@@ -73,18 +70,16 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 				receivedAt: new Date(2018, 0, 1).toISOString(),
 			});
 
-			await enqueue(amqpConnection, createEvent('event1', 3), 'destination2');
 			const result3 = await channel.get('consumer2_event1');
 			JSON.parse(result3!.content.toString()).should.deepEqual({
-				...createEvent('event1', 3),
+				...createEvent('event1', 1),
 				dispatchedAt: new Date(2018, 0, 1).toISOString(),
 				receivedAt: new Date(2018, 0, 1).toISOString(),
 			});
 
-			await enqueue(amqpConnection, createEvent('event1', 4), 'destination3');
-			const result4 = await channel.get('consumer3_event1');
+			const result4 = await channel.get('consumer3_event2');
 			JSON.parse(result4!.content.toString()).should.deepEqual({
-				...createEvent('event1', 4),
+				...createEvent('event2', 2),
 				dispatchedAt: new Date(2018, 0, 1).toISOString(),
 				receivedAt: new Date(2018, 0, 1).toISOString(),
 			});
@@ -95,16 +90,12 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 
 		it('should fetch next enqueued event', async function () {
 			const channel = await this.amqplibConnection.createChannel();
-			await channel.deleteQueue('destination1');
-			await channel.deleteQueue('destination2');
-			await channel.deleteQueue('destination3');
+			await channel.deleteExchange('events');
 			await channel.deleteQueue('consumer1_event1');
 			await channel.deleteQueue('consumer1_event2');
 			await channel.deleteQueue('consumer2_event1');
-			await channel.deleteQueue('consumer3_event1');
-			await channel.assertExchange('destination1', 'direct');
-			await channel.assertExchange('destination2', 'direct');
-			await channel.assertExchange('destination3', 'direct');
+			await channel.deleteQueue('consumer3_event2');
+			await channel.assertExchange('events', 'direct');
 			await channel.assertQueue('consumer1_event1', {
 				deadLetterExchange: '',
 				deadLetterRoutingKey: '__rejected.consumer1_event1',
@@ -117,34 +108,30 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 				deadLetterExchange: '',
 				deadLetterRoutingKey: '__rejected.consumer2_event1',
 			});
-			await channel.assertQueue('consumer3_event1', {
+			await channel.assertQueue('consumer3_event2', {
 				deadLetterExchange: '',
-				deadLetterRoutingKey: '__rejected.consumer3_event1',
+				deadLetterRoutingKey: '__rejected.consumer3_event2',
 			});
-			await channel.bindQueue('consumer1_event1', 'destination1', 'event1');
-			await channel.bindQueue('consumer1_event2', 'destination1', 'event2');
-			await channel.bindQueue('consumer2_event1', 'destination2', 'event1');
-			await channel.bindQueue('consumer3_event1', 'destination3', 'event1');
+			await channel.bindQueue('consumer1_event1', 'events', 'event1');
+			await channel.bindQueue('consumer1_event2', 'events', 'event2');
+			await channel.bindQueue('consumer2_event1', 'events', 'event1');
+			await channel.bindQueue('consumer3_event2', 'events', 'event2');
 
-			channel.publish('destination1', 'event1', new Buffer(JSON.stringify(createEvent('event1', 1))));
+			channel.publish('events', 'event1', new Buffer(JSON.stringify(createEvent('event1', 1))));
+			channel.publish('events', 'event2', new Buffer(JSON.stringify(createEvent('event2', 2))));
 			await wait(200);
-			const event1 = await fetchNext(amqpConnection, 'event1', 'destination1', 'consumer1');
+
+			const event1 = await fetchNext(amqpConnection, 'event1', 'consumer1');
 			event1!.should.deepEqual(createEvent('event1', 1));
 
-			channel.publish('destination1', 'event2', new Buffer(JSON.stringify(createEvent('event2', 2))));
-			await wait(200);
-			const event2 = await fetchNext(amqpConnection, 'event2', 'destination1', 'consumer1');
+			const event2 = await fetchNext(amqpConnection, 'event2', 'consumer1');
 			event2!.should.deepEqual(createEvent('event2', 2));
 
-			channel.publish('destination2', 'event1', new Buffer(JSON.stringify(createEvent('event1', 3))));
-			await wait(200);
-			const event3 = await fetchNext(amqpConnection, 'event1', 'destination2', 'consumer2');
-			event3!.should.deepEqual(createEvent('event1', 3));
+			const event3 = await fetchNext(amqpConnection, 'event1', 'consumer2');
+			event3!.should.deepEqual(createEvent('event1', 1));
 
-			channel.publish('destination3', 'event1', new Buffer(JSON.stringify(createEvent('event1', 4))));
-			await wait(200);
-			const event4 = await fetchNext(amqpConnection, 'event1', 'destination3', 'consumer3');
-			event4!.should.deepEqual(createEvent('event1', 4));
+			const event4 = await fetchNext(amqpConnection, 'event2', 'consumer3');
+			event4!.should.deepEqual(createEvent('event2', 2));
 		});
 	});
 
@@ -152,43 +139,35 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 
 		it('should call given callback everytime a specified event is enqueued for a specified destination', async function () {
 			const channel = await this.amqplibConnection.createChannel();
-			await channel.deleteQueue('destination1');
-			await channel.deleteQueue('destination2');
-			await channel.deleteQueue('destination3');
+			await channel.deleteExchange('events');
 			await channel.deleteQueue('consumer1_event1');
 			await channel.deleteQueue('consumer1_event2');
 			await channel.deleteQueue('consumer2_event1');
-			await channel.deleteQueue('consumer3_event1');
-			await channel.assertExchange('destination1', 'direct');
-			await channel.assertExchange('destination2', 'direct');
-			await channel.assertExchange('destination3', 'direct');
+			await channel.deleteQueue('consumer3_event2');
+			await channel.assertExchange('events', 'direct');
 
 			const callbackD1C1E1 = sinon.spy();
 			const callbackD1C1E2 = sinon.spy();
 			const callbackD2C2E1 = sinon.spy();
-			const callbackD3C3E1 = sinon.spy();
+			const callbackD3C3E2 = sinon.spy();
 
-			const cancel1 = await bindOne(amqpConnection, 'event1', 'destination1', 'consumer1', callbackD1C1E1);
-			const cancel2 = await bindOne(amqpConnection, 'event2', 'destination1', 'consumer1', callbackD1C1E2);
-			const cancel3 = await bindOne(amqpConnection, 'event1', 'destination2', 'consumer2', callbackD2C2E1);
-			const cancel4 = await bindOne(amqpConnection, 'event1', 'destination3', 'consumer3', callbackD3C3E1);
+			const cancel1 = await bindOne(amqpConnection, 'event1', 'consumer1', callbackD1C1E1);
+			const cancel2 = await bindOne(amqpConnection, 'event2', 'consumer1', callbackD1C1E2);
+			const cancel3 = await bindOne(amqpConnection, 'event1', 'consumer2', callbackD2C2E1);
+			const cancel4 = await bindOne(amqpConnection, 'event2', 'consumer3', callbackD3C3E2);
 
-			channel.publish('destination1', 'event1', new Buffer(JSON.stringify(createEvent('event1', 1))));
+			channel.publish('events', 'event1', new Buffer(JSON.stringify(createEvent('event1', 1))));
+			channel.publish('events', 'event2', new Buffer(JSON.stringify(createEvent('event2', 2))));
+
 			await waitUntil(async () => callbackD1C1E1.withArgs(createEvent('event1', 1)).calledOnce);
-
-			channel.publish('destination1', 'event2', new Buffer(JSON.stringify(createEvent('event2', 2))));
 			await waitUntil(async () => callbackD1C1E2.withArgs(createEvent('event2', 2)).calledOnce);
-
-			channel.publish('destination2', 'event1', new Buffer(JSON.stringify(createEvent('event1', 3))));
-			await waitUntil(async () => callbackD2C2E1.withArgs(createEvent('event1', 3)).calledOnce);
-
-			channel.publish('destination3', 'event1', new Buffer(JSON.stringify(createEvent('event1', 4))));
-			await waitUntil(async () => callbackD3C3E1.withArgs(createEvent('event1', 4)).calledOnce);
+			await waitUntil(async () => callbackD2C2E1.withArgs(createEvent('event1', 1)).calledOnce);
+			await waitUntil(async () => callbackD3C3E2.withArgs(createEvent('event2', 2)).calledOnce);
 
 			callbackD1C1E1.callCount.should.equal(1);
 			callbackD1C1E2.callCount.should.equal(1);
 			callbackD2C2E1.callCount.should.equal(1);
-			callbackD3C3E1.callCount.should.equal(1);
+			callbackD3C3E2.callCount.should.equal(1);
 
 			await cancel1();
 			await cancel2();
@@ -201,33 +180,25 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 
 		it('should call given callback on specified event enqueued and send response message, returned by the callback', async function () {
 			const channel = await this.amqplibConnection.createChannel();
-			await channel.deleteQueue('destination1');
-			await channel.deleteQueue('destination2');
-			await channel.deleteQueue('destination3');
+			await channel.deleteExchange('events');
 			await channel.deleteQueue('consumer1_event1');
 			await channel.deleteQueue('consumer1_event2');
 			await channel.deleteQueue('consumer2_event1');
-			await channel.deleteQueue('consumer3_event1');
 			await channel.deleteQueue('replyQueue1');
 			await channel.deleteQueue('replyQueue2');
 			await channel.deleteQueue('replyQueue3');
-			await channel.deleteQueue('replyQueue4');
-			await channel.assertExchange('destination1', 'direct');
-			await channel.assertExchange('destination2', 'direct');
-			await channel.assertExchange('destination3', 'direct');
+			await channel.assertExchange('events', 'direct');
 
 			const callback1 = sinon.stub().callsArg(1).resolves();
 			const callback2 = sinon.stub().callsArg(1).resolves();
 			const callback3 = sinon.stub().callsArg(1).resolves();
-			const callback4 = sinon.stub().callsArg(1).resolves();
 
-			const cancel1 = await bindOneExpectingConfirmation(amqpConnection, 'event1', 'destination1', 'consumer1', callback1);
-			const cancel2 = await bindOneExpectingConfirmation(amqpConnection, 'event2', 'destination1', 'consumer1', callback2);
-			const cancel3 = await bindOneExpectingConfirmation(amqpConnection, 'event1', 'destination2', 'consumer2', callback3);
-			const cancel4 = await bindOneExpectingConfirmation(amqpConnection, 'event1', 'destination3', 'consumer3', callback4);
+			const cancel1 = await bindOneExpectingConfirmation(amqpConnection, 'event1', 'consumer1', callback1);
+			const cancel2 = await bindOneExpectingConfirmation(amqpConnection, 'event2', 'consumer1', callback2);
+			const cancel3 = await bindOneExpectingConfirmation(amqpConnection, 'event3', 'consumer2', callback3);
 
 			channel.publish(
-				'destination1',
+				'events',
 				'event1',
 				new Buffer(JSON.stringify({ a: 1 })),
 				{ replyTo: 'replyQueue1', correlationId: 'test1' },
@@ -238,7 +209,7 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 			response1!.properties.correlationId.should.equal('test1');
 
 			channel.publish(
-				'destination1',
+				'events',
 				'event2',
 				new Buffer(JSON.stringify({ a: 2 })),
 				{ replyTo: 'replyQueue2', correlationId: 'test2' },
@@ -249,8 +220,8 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 			response2!.properties.correlationId.should.equal('test2');
 
 			channel.publish(
-				'destination2',
-				'event1',
+				'events',
+				'event3',
 				new Buffer(JSON.stringify({ a: 3 })),
 				{ replyTo: 'replyQueue3', correlationId: 'test3' },
 			);
@@ -259,21 +230,9 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 			should(response3).not.be.false();
 			response3!.properties.correlationId.should.equal('test3');
 
-			channel.publish(
-				'destination3',
-				'event1',
-				new Buffer(JSON.stringify({ a: 4 })),
-				{ replyTo: 'replyQueue4', correlationId: 'test4' },
-			);
-			await waitUntil(async () => callback4.calledOnce);
-			const response4 = await channel.get('replyQueue4');
-			should(response4).not.be.false();
-			response4!.properties.correlationId.should.equal('test4');
-
 			await cancel1();
 			await cancel2();
 			await cancel3();
-			await cancel4();
 		});
 	});
 
@@ -281,19 +240,10 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 
 		it('should purge all events of given types from given destination for a given consumer type', async function () {
 			const channel = await this.amqplibConnection.createChannel();
-			await channel.deleteQueue('destination1');
-			await channel.deleteQueue('destination2');
-			await channel.deleteQueue('destination3');
+			await channel.deleteExchange('events');
 			await channel.deleteQueue('consumer1_event1');
-			await channel.deleteQueue('consumer1_event2');
 			await channel.deleteQueue('consumer2_event1');
-			await channel.deleteQueue('consumer3_event1');
-			await channel.deleteQueue('replyQueue1');
-			await channel.deleteQueue('replyQueue2');
-			await channel.deleteQueue('replyQueue3');
-			await channel.deleteQueue('replyQueue4');
-			await channel.assertExchange('destination1', 'direct');
-			await channel.assertExchange('destination2', 'direct');
+			await channel.assertExchange('events', 'direct');
 			await channel.assertQueue('consumer1_event1', {
 				deadLetterExchange: '',
 				deadLetterRoutingKey: '__rejected.consumer1_event1',
@@ -302,15 +252,15 @@ describe('AMQP.EventSourcing.eventQueue', function () {
 				deadLetterExchange: '',
 				deadLetterRoutingKey: '__rejected.consumer2_event1',
 			});
-			await channel.bindQueue('consumer1_event1', 'destination1', 'event1');
-			await channel.bindQueue('consumer2_event1', 'destination2', 'event1');
+			await channel.bindQueue('consumer1_event1', 'events', 'event1');
+			await channel.bindQueue('consumer2_event1', 'events', 'event1');
 
-			channel.publish('destination1', 'event1', new Buffer(JSON.stringify(createEvent('event1', 1))));
-			await purgeMore(amqpConnection, ['event1'], 'destination1', 'consumer1');
+			channel.publish('events', 'event1', new Buffer(JSON.stringify(createEvent('event1', 1))));
+			await purgeMore(amqpConnection, ['event1'], 'consumer1');
 			(await channel.get('consumer1_event1')).should.be.false();
 
-			channel.publish('destination2', 'event1', new Buffer(JSON.stringify(createEvent('event1', 2))));
-			await purgeMore(amqpConnection, ['event1'], 'destination2', 'consumer2');
+			channel.publish('events', 'event1', new Buffer(JSON.stringify(createEvent('event1', 2))));
+			await purgeMore(amqpConnection, ['event1'], 'consumer2');
 			(await channel.get('consumer2_event1')).should.be.false();
 		});
 	});
