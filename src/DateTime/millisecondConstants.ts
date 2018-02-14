f527cbcd9d3d97c296ba3711b23c65799fe63ee2
Michael Zabka <zabka.michael@gmail.com>
2018-02-14 13:14:58 +0100

    Add milliseconds constants for better computions

---
+++ src/DateTime/millisecondConstants.ts
@@ -0,0 +1,8 @@
+
+export const SECOND_IN_MS = 1e3;
+export const MINUTE_IN_MS = 60 * SECOND_IN_MS;
+export const HOUR_IN_MS = 60 * MINUTE_IN_MS;
+export const DAY_IN_MS = 24 * HOUR_IN_MS;
+export const WEEK_IN_MS = 7 * DAY_IN_MS;
+export const NON_LEAP_YEAR_IN_MS = 365 * DAY_IN_MS;
+export const LEAP_YEAR_IN_MS = 366 * DAY_IN_MS;
