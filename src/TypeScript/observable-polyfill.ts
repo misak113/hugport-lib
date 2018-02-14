34002ec5c4c3f9000b32570373a1e6f20604b2da
Michael Zabka <zabka.michael@gmail.com>
2018-02-14 18:27:26 +0100

    Add general observable polyfill

---
+++ src/TypeScript/observable-polyfill.ts
@@ -0,0 +1,27 @@
+
+import * as Observable from 'zen-observable';
+
+let root;
+
+if (typeof self !== 'undefined') {
+	root = self;
+} else if (typeof window !== 'undefined') {
+	root = window;
+} else if (typeof global !== 'undefined') {
+	root = global;
+} else if (typeof module !== 'undefined') {
+	root = module;
+} else {
+	root = Function('return this')();
+}
+
+if (typeof root.Observable === 'undefined') {
+	root.Observable = Observable;
+	(Symbol as any).observable = Observable;
+}
+if (typeof root.Observable.prototype.map === 'undefined') {
+	root.Observable.prototype.map = Observable.prototype.map;
+}
+if (typeof root.Observable.prototype.filter === 'undefined') {
+	root.Observable.prototype.filter = Observable.prototype.filter;
+}
